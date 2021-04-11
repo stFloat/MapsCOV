e163bde852d9933cddc4062a953bb6063e6552e3
stFloat <a18513141418@163.com>
2021-04-11 10:27:53 +0800

    Create ArcMenu.java

---
+++ app/src/main/java/com/dongwanghan/mapscov/view/ArcMenu.java
@@ -0,0 +1,24 @@
+package com.dongwanghan.mapscov.view;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ArcMenu extends ViewGroup implements View.OnClickListener {
+    /*
+    * @Description: TODO
+    * @author: youyu
+    * @date:
+    *
+    
+    * @Return: 
+    */
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    @Override
+    protected void onLayout(boolean b, int i, int i1, int i2, int i3) {
+
+    }
+}
